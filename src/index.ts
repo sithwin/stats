3da7bfdb660eb4ab8cec9aef1b23b956fde5fe3e
banyarthein <banyarthein@gmail.com>
2022-09-25 21:24:45 +0800

    Ba Nyar's commit

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import fs from 'fs';
+import fs from "fs";
 
 const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
+  .readFileSync("football.csv", {
+    encoding: "utf-8",
   })
-  .split('\n')
+  .split("\n")
   .map((row: string): string[] => {
-    return row.split(',');
+    return row.split(",");
   });
 
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === "Man United" && match[5] === "H") {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === "Man United" && match[5] === "A") {
     manUnitedWins++;
   }
 }
 
-console.log('SI THU 5');
+console.log("SI THU 5");
+console.log("BNT Comment here");
 console.log(`Man United won ${manUnitedWins} games`);
